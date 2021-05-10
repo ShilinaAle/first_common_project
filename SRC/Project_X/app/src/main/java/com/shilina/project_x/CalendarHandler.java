7ac20e62f0c3586dbfe7257b6dfefb8c706e269d
KeyRi-Kun <47923763+KeyRi-Kun@users.noreply.github.com>
2021-05-11 01:58:40 +0300

    Refactored code

--- SRC/Project_X/app/src/main/java/com/shilina/project_x/CalendarHandler.java
+++ SRC/Project_X/app/src/main/java/com/shilina/project_x/CalendarHandler.java
@@ -2,56 +2,29 @@ package com.shilina.project_x;
 
 
 import android.Manifest;
-import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Color;
-import android.graphics.PixelFormat;
-import android.media.AudioManager;
 import android.net.Uri;
-import android.os.Build;
 import android.provider.CalendarContract;
 import android.provider.CalendarContract.Calendars;
 import android.provider.CalendarContract.Events;
-import android.telecom.TelecomManager;
-import android.telephony.SmsManager;
-import android.telephony.TelephonyManager;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.view.WindowManager.LayoutParams;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
-
 
 
 public class CalendarHandler {
 
-    public static String phoneNumber;
-
-
-    public static boolean calendarExist(Context context) {
+    public static boolean isCalendarExists(Context context) {
         Cursor cur = null;
         ContentResolver cr = context.getContentResolver();
         Uri uriCal = Calendars.CONTENT_URI;
@@ -60,55 +33,50 @@ public class CalendarHandler {
                 .appendQueryParameter(Calendars.ACCOUNT_NAME, "cal@projectx.com")
                 .appendQueryParameter(Calendars.ACCOUNT_TYPE, "com.projectx").build();
 
-        boolean checkAC = false;
         String[] projection = new String[] { Calendars.ACCOUNT_NAME, Calendars.ACCOUNT_TYPE};
         cur = cr.query(uriCal, projection, null, null, null);
         while (cur.moveToNext()) {
             String calType = cur.getString(cur.getColumnIndex(Calendars.ACCOUNT_TYPE));
-            Log.i("LOOK HERE: CalendarHandler", "\nType: " + calType);
             if (calType.equals("com.projectx")) {
-                checkAC = true;
+                Log.i("LOOK HERE: CalendarHandler", "Calendar exists");
+                cur.close();
+                return true;
             }
         }
-        return checkAC;
+        cur.close();
+        return false;
     }
 
     public static void addCalendar(Context context) {
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        Cursor cur = null;
-        ContentResolver cr = context.getContentResolver();
-        Uri uriCal = Calendars.CONTENT_URI;
-        if (calendarExist(context)) {
-            Log.i("LOOK HERE: PhoneStateReceiver", "ProjectX exists");
+        if (isCalendarExists(context)) {
             return;
         }
-        else {
-            Log.i("LOOK HERE: PhoneStateReceiver", "ProjectX is to be created");
-            ContentValues contentValues = new ContentValues();
-            contentValues.put(Calendars.ACCOUNT_NAME, "cal@projectx.com");
-            contentValues.put(Calendars.ACCOUNT_TYPE, "com.projectx");
-            contentValues.put(Calendars.NAME, "ProjectX Calendar");
-            contentValues.put(Calendars.CALENDAR_DISPLAY_NAME, "ProjectX Calendar");
-            contentValues.put(Calendars.CALENDAR_COLOR, Color.BLUE);
-            contentValues.put(Calendars.CALENDAR_ACCESS_LEVEL, Calendars.CAL_ACCESS_OWNER);
-            contentValues.put(Calendars.OWNER_ACCOUNT, "KGrigor98@gmail.com");
-            contentValues.put(Calendars.ALLOWED_REMINDERS, "METHOD_ALERT, METHOD_EMAIL, METHOD_ALARM");
-            contentValues.put(Calendars.ALLOWED_ATTENDEE_TYPES, "TYPE_OPTIONAL, TYPE_REQUIRED, TYPE_RESOURCE");
-            contentValues.put(Calendars.ALLOWED_AVAILABILITY, "AVAILABILITY_BUSY, AVAILABILITY_FREE, AVAILABILITY_TENTATIVE");
-            contentValues.put(Calendars.SYNC_EVENTS,1);
-            Uri.Builder builder = CalendarContract.Calendars.CONTENT_URI.buildUpon();
-            builder.appendQueryParameter(Calendars.ACCOUNT_NAME, "cal@projectx.com");
-            builder.appendQueryParameter(Calendars.ACCOUNT_TYPE, CalendarContract.ACCOUNT_TYPE_LOCAL);
-            builder.appendQueryParameter(CalendarContract.CALLER_IS_SYNCADAPTER,"true");
-            Uri uri = cr.insert(builder.build(), contentValues);
-        }
-        cur.close();
+        Log.i("LOOK HERE: CalendarHandler", "ProjectX is to be created");
+        ContentResolver cr = context.getContentResolver();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Calendars.ACCOUNT_NAME, "cal@projectx.com");
+        contentValues.put(Calendars.ACCOUNT_TYPE, "com.projectx");
+        contentValues.put(Calendars.NAME, "ProjectX Calendar");
+        contentValues.put(Calendars.CALENDAR_DISPLAY_NAME, "ProjectX Calendar");
+        contentValues.put(Calendars.CALENDAR_COLOR, Color.BLUE);
+        contentValues.put(Calendars.CALENDAR_ACCESS_LEVEL, Calendars.CAL_ACCESS_OWNER);
+        contentValues.put(Calendars.OWNER_ACCOUNT, "KGrigor98@gmail.com");
+        contentValues.put(Calendars.ALLOWED_REMINDERS, "METHOD_ALERT, METHOD_EMAIL, METHOD_ALARM");
+        contentValues.put(Calendars.ALLOWED_ATTENDEE_TYPES, "TYPE_OPTIONAL, TYPE_REQUIRED, TYPE_RESOURCE");
+        contentValues.put(Calendars.ALLOWED_AVAILABILITY, "AVAILABILITY_BUSY, AVAILABILITY_FREE, AVAILABILITY_TENTATIVE");
+        contentValues.put(Calendars.SYNC_EVENTS, 1);
+        Uri.Builder builder = CalendarContract.Calendars.CONTENT_URI.buildUpon();
+        builder.appendQueryParameter(Calendars.ACCOUNT_NAME, "cal@projectx.com");
+        builder.appendQueryParameter(Calendars.ACCOUNT_TYPE, CalendarContract.ACCOUNT_TYPE_LOCAL);
+        builder.appendQueryParameter(CalendarContract.CALLER_IS_SYNCADAPTER, "true");
+        cr.insert(builder.build(), contentValues);
     }
 
-    public static String getFreeTimeFromCalendar(Context context, Date startTime) {
-        long millis = startTime.getTime();
+    public static long getFreeTimeFromCalendar(Context context, long currentTime) {
+        addCalendar(context);
         long freeTimeMil = -1;
 
         Cursor cur = null;
@@ -122,16 +90,10 @@ public class CalendarHandler {
                 Events.ALL_DAY
         };
         String where = "((" + Events.DTSTART + " <= ?) AND (" + Events.DTEND + " >= ?))";
-        String[] equal = new String[] { Long.toString(millis), Long.toString(millis) };
-
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-            return null;
-        }
+        String[] equal = new String[] { Long.toString(currentTime), Long.toString(currentTime) };
 
         cur = cr.query(uri, from, where, equal, null);
-        //TODO: Разобраться с часовыми поясами
         while (cur.moveToNext()) {
-            Log.i("LOOK HERE: CalendarHandler", "cur is not null");
             long dtend = cur.getLong(cur.getColumnIndex(Events.DTEND));
             if ((dtend >= freeTimeMil) && (cur.getLong(cur.getColumnIndex(Events.ALL_DAY)) == 0)) {
                 freeTimeMil = dtend;
@@ -141,24 +103,24 @@ public class CalendarHandler {
         cur.close();
 
         if (freeTimeMil < 0) {
-            //return "Введите время";
             Log.i("LOOK HERE: CalendarHandler", "No current events");
-            return null;
         } else {
-            //return freeTimeStr;
-            Date d = new Date(freeTimeMil);
-            if (freeTimeMil != startTime.getTime()){
-                String s = getFreeTimeFromCalendar(context, new Date(freeTimeMil));
-                d = new Date(Long.parseLong(s));
+            if (freeTimeMil != currentTime){
+                Log.i("LOOK HERE: CalendarHandler", "Сравниваеются " + freeTimeMil + " и " + currentTime);
+                freeTimeMil = getFreeTimeFromCalendar(context, freeTimeMil);
             }
             Log.i("LOOK HERE: CalendarHandler", "Current event has been found");
-            return Long.toString(d.getTime());
         }
+        return freeTimeMil;
     }
 
-    public static String calendarId(Context context) {
+    public static boolean isFreeAt(Context context, long timeToCheck){
+        return (getFreeTimeFromCalendar(context, timeToCheck) == -1);
+    }
+
+    public static String getCalendarID(Context context) {
         ContentResolver cr = context.getContentResolver();
-        String freeTimeId = null;
+        String calendarID = null;
         Uri uriCal = Calendars.CONTENT_URI;
         uriCal = uriCal.buildUpon()
                 .appendQueryParameter(CalendarContract.CALLER_IS_SYNCADAPTER, "true")
@@ -171,27 +133,24 @@ public class CalendarHandler {
             String calType = curCal.getString(curCal.getColumnIndex(Calendars.ACCOUNT_TYPE));
             Log.i("LOOK HERE: CalendarHandler", "\nName: " + calType);
             if (calType.equals("com.projectx")) {
-                freeTimeId = curCal.getString(curCal.getColumnIndex(Calendars._ID));
+                calendarID = curCal.getString(curCal.getColumnIndex(Calendars._ID));
             }
         }
         curCal.close();
-        return freeTimeId;
+        return calendarID;
     }
 
-    public static void addEvent(Context context, String freeTime, String freeTimeTZ, String finalStartTimeStr) {
+    public static void addEvent(Context context, String phoneNumber, long callStartTime, long freeTime) {
+        addCalendar(context);
+        String descr = context.getResources().getString(R.string.textCalendar, phoneNumber, getTimeStringFromLong(callStartTime, "d.MM.y kk:mm"));
         ContentResolver cr = context.getContentResolver();
-        long freeTimeMil = Long.valueOf(freeTime);
-        long freeTimeEnd = freeTimeMil + 300000;
-        String freeTimeId = calendarId(context);
-        Date finalStartTimeDate = new Date(Long.parseLong(finalStartTimeStr));
-        Log.i("LOOK HERE: CalendarHandler", "Call was: " + finalStartTimeStr + "\nCalendarID: " + freeTimeId + "\nTimeZone: " + freeTimeTZ);
         ContentValues values = new ContentValues();
-        values.put(Events.DTSTART, freeTimeMil);
-        values.put(Events.DTEND, freeTimeEnd);
-        values.put(Events.TITLE, "Перенесенный звонок");
-        values.put(Events.DESCRIPTION, "Вам звонили с номера " + phoneNumber + " в " + toFormat(finalStartTimeDate, "kk:mm"));
-        values.put(Events.CALENDAR_ID, freeTimeId);
-        values.put(Events.EVENT_TIMEZONE, freeTimeTZ);
+        values.put(Events.DTSTART, freeTime);
+        values.put(Events.DTEND, freeTime + 300000);
+        values.put(Events.TITLE, "Запланированный звонок");
+        values.put(Events.DESCRIPTION, descr);
+        values.put(Events.CALENDAR_ID, getCalendarID(context));
+        values.put(Events.EVENT_TIMEZONE, TimeZone.getDefault().getID());
         values.put(Events.GUESTS_CAN_INVITE_OTHERS, "1");
         values.put(Events.GUESTS_CAN_SEE_GUESTS, "1");
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
@@ -199,14 +158,16 @@ public class CalendarHandler {
         }
         cr.insert(Events.CONTENT_URI, values);
         Toast.makeText(context, "Напоминание добавлено в календарь", Toast.LENGTH_SHORT).show();
-        Log.i("LOOK HERE: CalendarHandler", "Reminder was added to calendar");
+        Log.i("LOOK HERE: CalendarHandler", descr);
     }
 
-    public static String toFormat(Date SomeTimeDate, String p) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat(p); //Маска даты
-        //dateFormat.setTimeZone(TimeZone.getTimeZone("GMT+3:00"));
-        dateFormat.setTimeZone(TimeZone.getDefault());
-        Log.i("LOOK HERE: CalendarHandler", "Local TimeZone is: " + dateFormat.getTimeZone());
+    public static String getTimeStringFromDate(Date SomeTimeDate, String formatStyle) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(formatStyle); //Маска даты
+        //dateFormat.setTimeZone(TimeZone.getDefault());
         return dateFormat.format(SomeTimeDate);
+    }
+
+    public static String getTimeStringFromLong(long millis, String formatStyle) {
+        return  getTimeStringFromDate(new Date(millis), formatStyle);
     }
 }
